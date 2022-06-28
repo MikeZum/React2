8a5df49513360d1fca95d6b54d005303e467ee4b
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-28 18:35:31 +0800

    11 43

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import dbMenu from "./DBMenu";
+import { ListItem } from "./ListItem";
+
 
 const MenuStyled = styled.main`
 background-color: #ccc;
@@ -9,6 +11,14 @@ margin-top: 80px;
 
 export const Menu = () => (
     <MenuStyled>
+        <section>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger}/>
+        </section>
 
+        <section>
+            <h2>Закуски / Напитки</h2>
+            <ListItem itemList={dbMenu.other}/>
+        </section>
     </MenuStyled>
-)
+);
