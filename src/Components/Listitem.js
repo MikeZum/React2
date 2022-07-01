d6c6589675c232fd9d1a3687823ca58c7e2f510e
«Mike» <michail.v.tolmachev@gmail.com>
2022-07-01 20:45:20 +0800

    les06

--- src/Components/Listitem.js
+++ src/Components/Listitem.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const List = styled.ul`
     display: flex;
-    // justyfy-content: space-around;
-    justyfy-content: space-beetwen;
+    justyfy-content: space-around;
+    // justyfy-content: space-between;
     flex-wrap: wrap;
 `;
 
